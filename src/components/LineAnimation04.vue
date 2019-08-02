6b9da9b950a2e5086650ea2582b0571c6d8f4108
sakaihisae <sakai@1-10.com>
2019-08-02 18:06:51 +0900

    ボタンアニメーション追加

--- src/components/LineAnimation04.vue
+++ src/components/LineAnimation04.vue
@@ -61,7 +61,7 @@ export default {
 
   --borderColor: #a0aef2;
   $border-Color: var(--borderColor);
-
+  $all_duration: 600ms;
   .second_side,
   .top_bottom,
   .first_side,
@@ -80,6 +80,7 @@ export default {
     }
   }
 
+  $second_side_delay: 450ms;
   .second_side {
     &:before,
     &:after {
@@ -88,34 +89,63 @@ export default {
       transition: all 10ms ease;
     }
     &:before {
-      top: 0;
-      right: 0;
+      bottom: 57%;
+      left: 100%;
     }
     &:after {
-      bottom: 0;
-      left: 0;
+      top: 57%;
+      right: 100%;
     }
-    &:hover::after,
+    &:hover::after {
+      animation: second_side_animation $all_duration $second_side_delay forwards, second_top_move $all_duration $second_side_delay forwards
+    }
     &:hover::before {
-      height: 0;
-      animation-name: second_side_animation;
-      animation-duration: 200ms;
-      animation-fill-mode: forwards;
-      animation-delay: 400ms;
+      animation: second_side_animation $all_duration $second_side_delay forwards, second_bottom_move $all_duration $second_side_delay forwards
     }
     @keyframes second_side_animation {
       0% {
-        height: 0%;
+        height: 43%;
       }
       20% {
         height: 0%;
       }
-
+      80% {
+        height: 0%;
+      }
       100% {
         height: 43%;
       }
     }
+    @keyframes second_top_move {
+      0% {
+        top: 57%;
+      }
+      20% {
+        top: 57%;
+      }
+      80% {
+        top: 100%;
+      }
+      100% {
+        top: 57%;
+      }
+    }
+    @keyframes second_bottom_move {
+      0% {
+        bottom: 57%;
+      }
+      20% {
+        bottom: 57%;
+      }
+      80% {
+        bottom: 100%;
+      }
+      100% {
+        bottom: 57%;
+      }
+    }
 
+    $top_bottom_delay: 300ms;
     .top_bottom {
       &:before,
       &:after {
@@ -124,34 +154,63 @@ export default {
         transition: all 10ms ease;
       }
       &:before {
-        top: 0;
+        bottom: 0;
         left: 0;
       }
       &:after {
-        bottom: 0;
+        top: 0;
         right: 0;
       }
-      &:hover::after,
+      &:hover::after {
+        animation: top_bottom_animation $all_duration $top_bottom_delay forwards, tb_right_move $all_duration $top_bottom_delay forwards;
+      }
       &:hover::before {
-        width: 0;
-        animation-name: top_bottom_animation;
-        animation-duration: 200ms;
-        animation-fill-mode: forwards;
-        animation-delay: 300ms;
+        animation: top_bottom_animation $all_duration $top_bottom_delay forwards, tb_left_move $all_duration $top_bottom_delay forwards;
       }
       @keyframes top_bottom_animation {
         0% {
-          width: 0%;
+          width: 100%;
         }
         20% {
           width: 0%;
         }
-
+        80% {
+          width: 0%;
+        }
         100% {
           width: 100%;
         }
       }
+      @keyframes tb_right_move {
+        0% {
+          right: 0%;
+        }
+        20% {
+          right: 0%;
+        }
+        80% {
+          right: 100%;
+        }
+        100% {
+          right: 0%;
+        }
+      }
+      @keyframes tb_left_move {
+        0% {
+          left: 0%;
+        }
+        20% {
+          left: 0%;
+        }
+        80% {
+          left: 100%;
+        }
+        100% {
+          left: 0%;
+        }
+      }
 
+      $first_side_delay: 150ms;
       .first_side {
         &:before,
         &:after {
@@ -160,31 +219,59 @@ export default {
           transition: all 10ms ease;
         }
         &:before {
-          top: 50%;
-          right: 0;
+          bottom: 0%;
+          left: 100%;
         }
         &:after {
-          bottom: 50%;
-          left: 0;
+          top: 0%;
+          right: 100%;
         }
-        &:hover::after,
+        &:hover::after {
+          animation: first_side_animation $all_duration $first_side_delay forwards, first_top_move $all_duration $first_side_delay forwards;
+        }
         &:hover::before {
-          height: 0%;
-          animation-name: first_side_animation;
-          animation-duration: 200ms;
-          animation-fill-mode: forwards;
-          animation-delay: 200ms;
+          animation: first_side_animation $all_duration $first_side_delay forwards, first_bottom_move $all_duration $first_side_delay forwards;
         }
         @keyframes first_side_animation {
           0% {
-            height: 0%;
+            height: 50%;
           }
           20% {
             height: 0%;
           }
-
+          80% {
+            height: 0%;
+          }
           100% {
             height: 50%;
+          }
+        }
+        @keyframes first_top_move {
+          0% {
+            top: 0%;
+          }
+          20% {
+            top: 0%;
+          }
+          80% {
+            top: 50%;
+          }
+          100% {
+            top: 0%;
+          }
+        }
+        @keyframes first_bottom_move {
+          0% {
+            bottom: 0%;
+          }
+          20% {
+            bottom: 0%;
+          }
+          80% {
+            bottom: 50%;
+          }
+          100% {
+            bottom: 0%;
           }
         }
 
@@ -200,33 +287,61 @@ export default {
             transition: all 10ms ease;
           }
           &:before {
-            top: 49%;
-            right: -49px;
+            top: 50%;
+            left: 100%;
           }
           &:after {
             top: 50%;
-            left: -49px;
+            right: 100%;
           }
-          &:hover::after,
           &:hover::before {
-            width: 0;
-            animation-name: side_line_animation;
-            animation-duration: 150ms;
-            animation-fill-mode: forwards;
+            animation: side_line_animation $all_duration forwards, side_left_move $all_duration forwards;
+          }
+          &:hover::after {
+            animation: side_line_animation $all_duration forwards, side_right_move $all_duration forwards;
           }
           @keyframes side_line_animation {
             0% {
-              width: 0px;
+              width: 50px;
             }
             20% {
               width: 0px;
             }
-
+            81% {
+              width: 0px;
+            }
             100% {
               width: 50px;
             }
           }
-
+          @keyframes side_left_move {
+            0% {
+              left: 100%;
+            }
+            20% {
+              left: 100%;
+            }
+            80% {
+              left: 150%;
+            }
+            100% {
+              left: 100%;
+            }
+          }
+          @keyframes side_right_move {
+            0% {
+              right: 100%;
+            }
+            20% {
+              right: 100%;
+            }
+            80% {
+              right: 150%;
+            }
+            100% {
+              right: 100%;
+            }
+          }
 
           a {
             --fontSize: 15px;
