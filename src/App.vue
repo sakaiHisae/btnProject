a1cf4944158626c412420d311a39a8ce63bc22e1
sakaihisae <sakai@1-10.com>
2019-08-05 16:46:53 +0900

    名前変更

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <GradationBtn msg="sample" url="#" :color="''" :fontSize="''" :padding="''" :bgGradation="''" :borderColor="''"/>
-    <LineAnimation msg="lineAnimation" url="#" :bgGradation="''" :borderColor="''"/>
+    <LineAnimation00 msg="lineAnimation00" url="#" :color="''" :fontSize="''" :padding="''" :bgGradation="''" :borderColor="''"/>
+    <LineAnimation01 msg="lineAnimation01" url="#" :bgGradation="''" :borderColor="''"/>
     <LineAnimation02 msg="lineAnimation02" url="#"/>
-    <LineAnimation03 msg="副産物" url="#"/>
-    <LineAnimation04 msg="test" url="#"/>
+    <LineAnimation03 msg="lineAnimation03" url="#"/>
+    <LineAnimation04 msg="lineAnimation04" url="#"/>
   </div>
 </template>
 
 <script>
-import GradationBtn from './components/gradation_btn.vue'
-import LineAnimation from './components/lineanimation_btn.vue'
+import LineAnimation00 from './components/LineAnimation00.vue'
+import LineAnimation01 from './components/LineAnimation01.vue'
 import LineAnimation02 from './components/LineAnimation02.vue'
 import LineAnimation03 from './components/LineAnimation03.vue'
 import LineAnimation04 from './components/LineAnimation04.vue'
@@ -18,8 +18,8 @@ import LineAnimation04 from './components/LineAnimation04.vue'
 export default {
   name: 'app',
   components: {
-    GradationBtn,
-    LineAnimation,
+    LineAnimation00,
+    LineAnimation01,
     LineAnimation02,
     LineAnimation03,
     LineAnimation04
